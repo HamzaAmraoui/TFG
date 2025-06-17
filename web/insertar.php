84bc5ba395f2f143a4ecce6dde980c27c07d07a7
HamzaAmraoui <80313251+HamzaAmraoui@users.noreply.github.com>
2025-06-17 22:34:54 +0200

    Update insertar.php

--- web/insertar.php
+++ web/insertar.php
@@ -36,31 +36,29 @@ table {
 <center><img src="banner.jpg" width ="800"><br><br><br>
 
 <?php
-$pro = $_GET['pro'];
-$pre = $_GET['pre'];
-$alm = $_GET['alm'];
-$cod = $_GET['cod'];
-$exi= $_GET['exi'];
-$cod= $_GET['cod'];
-$exi1= $_GET['exi1'];
+$marca = $_GET['pro'];
+$modelo = $_GET['pre'];
+$version = $_GET['alm'];
+$precio = $_GET['exi'];
+$garantia = $_GET['gar'];   
+$codigo = $_GET['cod'];
+$existencias = $_GET['exi1'];
 
-$con=mysqli_connect("db","root","root","concesionario");
+$con = mysqli_connect("db", "root", "root", "concesionario");
 
-if (!$con)
-{
-die('Error en la conexión : ');
+if (!$con) {
+    die('Error en la conexión : ' . mysqli_connect_error());
 }
+
 $query = "INSERT INTO coches (marca, modelo, version, precio, garantia, codigo, existencias) 
           VALUES ('$marca', '$modelo', '$version', $precio, $garantia, '$codigo', $existencias)";
 
 $result = $con->query($query);
 
-if($con ->affected_rows == 1){
-
-    header ('location:index.php');
-}else{
-    echo "No se ha metido los datos";
+if ($con->affected_rows == 1) {
+    header('Location: index.php');
+} else {
+    echo "No se ha podido insertar el coche.";
 }
-
 ?>
 </body>
